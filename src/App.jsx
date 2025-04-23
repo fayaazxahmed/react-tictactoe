1f896cd9c50f4be8520b389fd47194d371cae945
Fayaaz Ahmed <fayaaz.ahmed.20@gmail.com>
2025-04-22 23:08:02 -0400

    Handle clicks

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,35 @@ import './App.css';
 
 function App() {
   const [turn, setTurn] = useState('X');
+  const [board, setBoard] = useState(Array(3).fill().map(() => Array(3).fill("")))
 
-  const handleClick = () => {
-    setTurn(turn === 'X' ? 'O' : 'X')
+  const handleClick = (row, col) => {
+    if (board[row][col] !== "") return; // Skip filled cells
+
+    const newBoard = board.map((r, rowIndex) =>
+      r.map((cell, colIndex) => {
+        if (rowIndex === row && colIndex === col) return turn;
+        return cell;
+      })
+    );
+
+    setBoard(newBoard);
+    console.log("Clicked cell:", row, col);
+    setTurn(turn === "X" ? "O" : "X");
   }
 
   return (
     <div style={{backgroundColor: "#06132b", minHeight: "100vh", paddingTop: "2rem"}}>
-      <table className='center'>
-        <thead>
-        <tr>
-            <td id="R1C1" onClick={handleClick}></td>
-            <td id="R1C2" onClick={handleClick}></td>
-            <td id="R1C3" onClick={handleClick}></td>
-        </tr>
-        <tr>
-            <td id="R2C1" onClick={handleClick}></td>
-            <td id="R2C2" onClick={handleClick}></td>
-            <td id="R2C3" onClick={handleClick}></td>
-        </tr>
-        <tr>
-            <td id="R3C1" onClick={handleClick}></td>
-            <td id="R3C2" onClick={handleClick}></td>
-            <td id="R3C3" onClick={handleClick}></td>
-        </tr>
-        </thead>
+      <table className='center board'>
+        <tbody>
+        {[0, 1, 2].map((row) => (
+          <tr key={row} >
+            {[0, 1, 2].map((col) => (
+              <td key={col} onClick={()=>handleClick(row,col)}></td>
+            ))}
+          </tr>
+        ))}
+        </tbody>
       </table>
       <div>Current Turn: <span>{turn}</span></div>
     </div>
