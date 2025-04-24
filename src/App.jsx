0cf4e7fac4c4708b94a85c716ec8a623b39b47c6
Fayaaz Ahmed <fayaaz.ahmed.20@gmail.com>
2025-04-24 10:50:43 -0400

    Update board after each turn

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   const [turn, setTurn] = useState('X');
   const [board, setBoard] = useState(Array(3).fill().map(() => Array(3).fill("")))
-
+  const [countMoves, setCountMoves] = useState(0);
+  
   const handleClick = (row, col) => {
     if (board[row][col] !== "") return; // Skip filled cells
 
@@ -15,19 +16,20 @@ function App() {
       })
     );
 
+    setCountMoves(countMoves => countMoves + 1);
     setBoard(newBoard);
-    console.log("Clicked cell:", row, col);
     setTurn(turn === "X" ? "O" : "X");
+    console.log(countMoves);
   }
 
   return (
-    <div style={{backgroundColor: "#06132b", minHeight: "100vh", paddingTop: "2rem"}}>
+    <div style={{backgroundColor: "#06132b", minHeight: "100vh", paddingTop: "6rem"}}>
       <table className='center board'>
         <tbody>
         {[0, 1, 2].map((row) => (
           <tr key={row} >
             {[0, 1, 2].map((col) => (
-              <td key={col} onClick={()=>handleClick(row,col)}></td>
+              <td key={col} onClick={()=>handleClick(row,col)} className={board[row][col] === "X" ? "PlaceX" : "PlaceO"}>{board[row][col]}</td>
             ))}
           </tr>
         ))}
