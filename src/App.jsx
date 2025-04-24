bd8f97173a15e999ad45e4e9264dc3e0e533fd56
Fayaaz Ahmed <fayaaz.ahmed.20@gmail.com>
2025-04-24 15:49:24 -0400

    Detect wins

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,29 @@ function App() {
   const [turn, setTurn] = useState('X');
   const [board, setBoard] = useState(Array(3).fill().map(() => Array(3).fill("")))
   const [countMoves, setCountMoves] = useState(0);
+  const [gameOver, setgameOver] = useState(false);
+  const [winner, setWinner] = useState("");
+  const [checkTie, setCheckTie] = useState(false);
   
+  const checkWin = (board, turn) => {
+    let i = 0;
+    for (i = 0; i < 3; i++) {
+      if (((board[i][0] === board[i][1]) && (board[i][0]=== board[i][2]) && (board[i][0] !== "")) || 
+      ((board[0][i] === board[1][i]) && (board[0][i]=== board[2][i]) && (board[0][i] !== "")) ||
+      ((board[0][0] === board[1][1]) && (board[1][1]=== board[2][2]) && (board[0][0] !== "")) ||
+      ((board[2][0] === board[1][1]) && (board[1][1]=== board[0][2]) && (board[2][0] !== ""))) {
+        setgameOver(gameOver => true);
+        setWinner(winner => "");
+        return true;
+      }
+    }
+  }
+
   const handleClick = (row, col) => {
-    if (board[row][col] !== "") return; // Skip filled cells
+    if (board[row][col] !== "" || checkWin(board, turn) === true) {
+      if (checkWin(board, turn)) console.log((turn === "X" ? "O" : "X"), " wins");
+      return; // Skip filled cells
+    }
 
     const newBoard = board.map((r, rowIndex) =>
       r.map((cell, colIndex) => {
@@ -15,7 +35,7 @@ function App() {
         return cell;
       })
     );
-
+    
     setCountMoves(countMoves => countMoves + 1);
     setBoard(newBoard);
     setTurn(turn === "X" ? "O" : "X");
@@ -36,6 +56,7 @@ function App() {
         </tbody>
       </table>
       <div>Current Turn: <span>{turn}</span></div>
+      <div></div>
     </div>
   );
 }
